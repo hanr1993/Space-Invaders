4b9568f19f51c65176db16455cbe1d1694e012f6
hanr1993 <92591105+hanr1993@users.noreply.github.com>
2022-08-17 11:53:20 +0100

    added in score board and game instructions

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const state = {
     36, 37, 38, 39, 40, 41, 48, 49, 50, 51, 52, 53, 54, 55, 56,
   ],
   gameOver: false,
+  score: 0,
 };
 
 const setGameUp = (element) => {
@@ -19,6 +20,7 @@ const setGameUp = (element) => {
   // draw the aliens
   drawAliens();
   // add the instructions and the score
+  drawScoreboard();
 };
 
 const drawGrid = () => {
@@ -97,6 +99,8 @@ const fire = () => {
       state.alienPosition.splice(state.alienPosition.indexOf(laserPosition), 1);
       state.cells[laserPosition].classList.remove("alien", "laser");
       state.cells[laserPosition].classList.add("boom");
+      state.score++;
+      state.scoreElement.innerText = state.score;
       setTimeout(() => {
         state.cells[laserPosition].classList.remove("boom");
       }, 200);
@@ -182,6 +186,27 @@ const drawMessage = (message) => {
   messageElement.append(h1);
   state.element.append(messageElement);
 };
+
+const drawScoreboard = () => {
+  const heading = document.createElement("h1");
+  heading.innerText = "Space Invaders";
+  const paragraph1 = document.createElement("p");
+  paragraph1.innerText = "Press SPACE to shoot.";
+  const paragraph2 = document.createElement("p");
+  paragraph2.innerText = "Press ← and → to move";
+  const scoreboard = document.createElement("div");
+  scoreboard.classList.add("scoreboard");
+  const scoreElement = document.createElement("span");
+  scoreElement.innerText = state.score;
+  const heading3 = document.createElement("h3");
+  heading3.innerText = "Score: ";
+  heading3.append(scoreElement);
+  scoreboard.append(heading, paragraph1, paragraph2, heading3);
+
+  state.scoreElement = scoreElement;
+  state.element.append(scoreboard);
+};
+
 // query the page for the place to insert my game
 const appElement = document.querySelector(".app");
 
